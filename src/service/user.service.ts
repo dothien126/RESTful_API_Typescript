46edcf229415768b28ff6ecc129e11c95b3fd771
dothien126 <dothien2601ak39@gmail.com>
2022-04-25 15:19:28 +0700

    feat: session controller - next

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,4 +1,5 @@
 import { DocumentDefinition } from "mongoose";
+import { omit } from "lodash";
 import UserModel, {UserDocument} from '../models/user.model'
 
 export async function createUser(input: DocumentDefinition<Omit<UserDocument, 'createdAt' | 'updatedAt'| 'comparePassword'>>) {
@@ -8,3 +9,27 @@ export async function createUser(input: DocumentDefinition<Omit<UserDocument, 'c
         throw new Error(error)   
     }
 }
+
+export async function validatePassword({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    const user = await UserModel.findOne({ email });
+  
+    if (!user) {
+      return false;
+    }
+  
+    const isValid = await user.comparePassword(password);
+  
+    if (!isValid) return false;
+  
+    return omit(user.toJSON(), "password");
+  }
+  
+  export async function findUser(query: FilterQuery<UserDocument>) {
+    return UserModel.findOne(query).lean();
+  }
